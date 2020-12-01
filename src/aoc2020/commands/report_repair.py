7b9caa48802e1a9646c190ddeb94333b89fe2e10
Jon Bringhurst <jon@bringhurst.org>
2020-12-01 09:28:55 -0800

    Add a basic two sum

--- src/aoc2020/commands/report_repair.py
+++ src/aoc2020/commands/report_repair.py
@@ -1,3 +1,5 @@
+from typing import Dict, List, Set
+
 import click
 
 
@@ -10,3 +12,17 @@ def report_repair() -> None:
 @report_repair.command("foo")
 def report_repair_foo() -> None:
     print("foo cmd here")
+
+
+def calc_two_sum(inputs: List[int], sum_to: int) -> Set[int]:
+    lookup: Dict[int, int] = dict()
+
+    for i in inputs:
+        lookup[sum_to - i] = i
+
+    for i in inputs:
+        j = lookup[i]
+        if i + j == sum_to:
+            return set({i, j})
+
+    raise ValueError("Two sum does not exist")
---
+++ tests/aoc2020/commands/test_report_repair.py
@@ -0,0 +1,13 @@
+from typing import List, Set
+
+from aoc2020.commands import report_repair
+
+
+def test_two_sum():
+    mock_input: List[int] = [1721, 979, 366, 299, 675, 1456]
+
+    mock_sum_to: int = 2020
+
+    mock_result: Set[int] = {1721, 299}
+
+    assert report_repair.calc_two_sum(mock_input, mock_sum_to) == mock_result
